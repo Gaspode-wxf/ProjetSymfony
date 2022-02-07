a3913d7f6d029c5c286c52bd73b447ed2427a710
AlexandreXXX <alexandre.mchich2021@campus-eni.fr>
2022-02-07 15:33:25 +0100

    Création des pages d'acceuil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,19 +7,30 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/", name="home_")
+ * @Route("/", name="main_")
  */
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("", name="index")
+     * @Route("accueil/deconecte", name="")
      */
-    public function index(): Response
+    public function accueilDeconnecte(): Response
     {
 
 
-        return $this->render('home/home.html.twig', [
+        return $this->render('main/accueil/deconecte.html.twig', [
+
+        ]);
+    }
+    /**
+     * @Route("accueil/conecte", name="")
+     */
+    public function accueilConnecte(): Response
+    {
+
+
+        return $this->render('main/accueil/conecte.html.twig', [
 
         ]);
     }
