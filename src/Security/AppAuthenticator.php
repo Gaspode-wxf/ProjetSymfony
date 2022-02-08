5d414be6514b04bb08d1b2b2adee364d66af952a
laurane <laurane.marie2021@campus-eni.fr>
2022-02-08 21:43:28 +0100

    ON PEUT SE COOOOOOOOOOOOOOO

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -2,12 +2,13 @@
 
 namespace App\Security;
 
-use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
@@ -23,16 +24,22 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     public const LOGIN_ROUTE = 'app_login';
 
     private UrlGeneratorInterface $urlGenerator;
+    private $router;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator, EntityManagerInterface $entityManager)
+    public function __construct(UrlGeneratorInterface $urlGenerator)
     {
         $this->urlGenerator = $urlGenerator;
     }
-
+    public function supports(Request $request): bool
+    {
+        return self::LOGIN_ROUTE===$request->attributes->get('_route')
+            && $request->isMethod('POST');
+    }
 
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
+
 
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
@@ -62,9 +69,13 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
     }
-    public function supports(Request $request): bool
-   {
-       return $request->isMethod('POST') && $this->getLoginUrl($request) === $request->getRequestUri();
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
+    {
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse(
+            $this->router->generate('app_login')
+        );
     }
 
 
