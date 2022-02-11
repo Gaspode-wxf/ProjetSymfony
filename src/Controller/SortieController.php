041f08945e3c66b3eba3dd8d9d977c97c023300f
laurane <laurane.marie2021@campus-eni.fr>
2022-02-11 18:45:14 +0100

    L'edit de sortie en creation et quand on est organisateur c'est bon

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\SortieType;
@@ -12,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function PHPUnit\Framework\throwException;
 
 /**
  * @Route("/sortie", name="sortie_")
@@ -126,9 +128,12 @@ class SortieController extends AbstractController
         /**
          * @Route("/{id}/edit", name="edit", methods={"GET", "POST"})
          */
-        public
-        function edit(Request $request, Sortie $sortie, EntityManagerInterface $entityManager): Response
+        public function edit(Request $request,
+                             Sortie $sortie,
+                             EntityManagerInterface $entityManager): Response
         {
+
+
             $form = $this->createForm(SortieType::class, $sortie);
             $form->handleRequest($request);
 
